34b86b81c40808cae1ffbf191df9d1739ec80f52
Dan Fritchman <fritchman@apple.com>
2018-11-17 14:43:37 -0800

    initial main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,32 @@
 // Created by Dan Fritchman on 11/17/18.
 //
 
-int main (){
+#include <iostream>
+
+template<typename T>
+void f(const T &p) {
+    T p3 = p * 3;
+    std::cout << p3 << std::endl;
+}
+
+void test_templates() {
+    int j = 1;
+    f(j);
+    long k = 2;
+    f(k);
+    float l = 3.0;
+    f(l);
+    double m = 4.0;
+    f(m);
+}
+
+int main() {
+    // Tell the people we alive
+    std::cout << "Hello World" << std::endl;
+
+    // Run all this stuff
+    test_templates();
+
+    // And bounce
     return 0;
 }
